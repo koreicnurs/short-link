73d02254bc105a710819293a70bfeeaf27f54010
koreicnurs <koreicnurs@gmail.com>
2022-09-08 17:02:36 +0600

    refactoring code add style css

--- frontend/src/containers/ShortLink/ShortLink.js
+++ frontend/src/containers/ShortLink/ShortLink.js
@@ -2,10 +2,13 @@ import React, {useState} from 'react';
 import {Button, Grid, Link, TextField} from "@mui/material";
 import {useDispatch, useSelector} from "react-redux";
 import {createLink} from "../../store/actions/linkActions";
+import Spinner from "../../components/UI/Spinner/Spinner";
+import './ShorLink.css';
 
 const ShortLink = () => {
     const dispatch = useDispatch();
     const link = useSelector(state => state.linkCombine.link);
+    const loading = useSelector(state => state.linkCombine.loading);
 
     const [state, setState] = useState({
         originalUrl: "",
@@ -24,7 +27,7 @@ const ShortLink = () => {
         });
     };
 
-    return (
+    return loading ? <Spinner/> : (
         <>
             <form
                 autoComplete="off"
@@ -56,7 +59,7 @@ const ShortLink = () => {
                     </Grid>
                 </Grid>
             </form>
-            {link ? <Link href={`${link.originalUrl}`}>{`http://localhost:8000/${link.shortUrl}`}</Link> : null}
+            {link ? <Link href={`${link.originalUrl}`} className='url-link'>{`http://localhost:8000/${link.shortUrl}`}</Link> : null}
         </>
 
     );
--- frontend/src/store/actions/linkActions.js
+++ frontend/src/store/actions/linkActions.js
@@ -1,33 +1,12 @@
 import axiosApi from "../../axiosApi";
 
-export const FETCH_LINK_REQUEST = 'FETCH_LINK_REQUEST';
-export const FETCH_LINK_SUCCESS = 'FETCH_LINK_SUCCESS';
-export const FETCH_LINK_FAILURE = 'FETCH_LINK_FAILURE';
-
 export const CREATE_LINK_REQUEST = 'CREATE_LINK_REQUEST';
 export const CREATE_LINK_SUCCESS = 'CREATE_LINK_SUCCESS';
 export const CREATE_LINK_FAILURE = 'CREATE_LINK_FAILURE';
 
-const fetchLinkRequest = () => ({type: FETCH_LINK_REQUEST});
-const fetchLinkSuccess = link => ({type: FETCH_LINK_SUCCESS, payload: link});
-const fetchLinkFailure = error => ({type: FETCH_LINK_FAILURE, payload: error});
-
 const createLinkRequest = () => ({type: CREATE_LINK_REQUEST});
 const createLinkSuccess = link => ({type: CREATE_LINK_SUCCESS, payload: link});
 const createLinkFailure = error => ({type: CREATE_LINK_FAILURE, payload: error});
-
-export const getLink = () => {
-    return async dispatch => {
-        try {
-            dispatch(fetchLinkRequest());
-
-            const response = await axiosApi(`/`);
-            dispatch(fetchLinkSuccess(response.data));
-        } catch (e) {
-            dispatch(fetchLinkFailure(e.message));
-        }
-    }
-};
 
 export const createLink = (data) => {
     return async dispatch => {
